4684f531baa954092b05b0d918d72a1aad7552bf
Mohit Patil <mohitpatil546@gmail.com>
2015-12-31 19:19:06 +0530

    nav bar and slider is added to index

--- dashboard_symanticUI/public/js/controllers/mainController.js
+++ dashboard_symanticUI/public/js/controllers/mainController.js
@@ -1,4 +1,12 @@
 angular.module('app.controllers', [])
 .controller('mainControllerCtrl', function($scope){
 
+
+	$('.ui.dropdown').dropdown();
+
+	$scope.toggleSidebar = function(){
+		$('.ui.labeled.icon.sidebar')
+			.sidebar('toggle')
+		;
+	};
 });
