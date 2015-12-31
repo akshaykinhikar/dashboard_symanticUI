5483faff1c32f812c2767f3d6e2ff9ba620cdc00
Mohit Patil <mohitpatil546@gmail.com>
2015-12-31 17:14:40 +0530

    bower install ui-router also 	index app and controller is injected

--- dashboard_symanticUI/public/js/app.js
+++ dashboard_symanticUI/public/js/app.js
@@ -1,4 +1,4 @@
-angular.module('app', ['app.controllers', 'app.services'])
-.config(function(){
+angular.module('app', ['app.controllers', 'ui.router'])
+.config(function($stateProvider, $urlRouterProvider){
 
 });
